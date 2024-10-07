7d614bd8238279c065fd83934e562c3ee546d6e7
S.A Mirghaderi <66771579+Mast3r0mid@users.noreply.github.com>
2024-10-08 00:40:05 +0100

    Add project files.

---
+++ MPC History Cleaner/Form1.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        ClearBtn = New Button()
+        ListBox1 = New ListBox()
+        SelectedLBL = New Label()
+        HistoryCountLBL = New Label()
+        SuspendLayout()
+        ' 
+        ' ClearBtn
+        ' 
+        ClearBtn.Anchor = AnchorStyles.Bottom Or AnchorStyles.Right
+        ClearBtn.Location = New Point(384, 326)
+        ClearBtn.Name = "ClearBtn"
+        ClearBtn.Size = New Size(75, 23)
+        ClearBtn.TabIndex = 3
+        ClearBtn.Text = "Clean"
+        ClearBtn.UseVisualStyleBackColor = True
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        ListBox1.FormattingEnabled = True
+        ListBox1.HorizontalScrollbar = True
+        ListBox1.ItemHeight = 15
+        ListBox1.Location = New Point(12, 20)
+        ListBox1.Name = "ListBox1"
+        ListBox1.SelectionMode = SelectionMode.MultiSimple
+        ListBox1.Size = New Size(447, 289)
+        ListBox1.TabIndex = 4
+        ' 
+        ' SelectedLBL
+        ' 
+        SelectedLBL.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left
+        SelectedLBL.AutoSize = True
+        SelectedLBL.Location = New Point(12, 330)
+        SelectedLBL.Name = "SelectedLBL"
+        SelectedLBL.Size = New Size(95, 15)
+        SelectedLBL.TabIndex = 5
+        SelectedLBL.Text = "Selected items: 0"
+        ' 
+        ' HistoryCountLBL
+        ' 
+        HistoryCountLBL.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        HistoryCountLBL.AutoSize = True
+        HistoryCountLBL.Location = New Point(138, 330)
+        HistoryCountLBL.Name = "HistoryCountLBL"
+        HistoryCountLBL.Size = New Size(27, 15)
+        HistoryCountLBL.TabIndex = 6
+        HistoryCountLBL.Text = "All :"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(471, 361)
+        Controls.Add(HistoryCountLBL)
+        Controls.Add(SelectedLBL)
+        Controls.Add(ListBox1)
+        Controls.Add(ClearBtn)
+        MaximizeBox = False
+        MinimumSize = New Size(300, 200)
+        Name = "Form1"
+        Text = "MPC History"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents ClearBtn As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents SelectedLBL As Label
+    Friend WithEvents HistoryCountLBL As Label
+
+End Class
---
+++ MPC History Cleaner/Form1.vb
@@ -0,0 +1,54 @@
+﻿Imports System
+Imports System.Windows.Forms.VisualStyles
+Imports Microsoft.VisualBasic.Devices
+Imports Microsoft.Win32
+Imports System.Linq
+Public Class Form1
+    Dim myDictionary As New Dictionary(Of String, String)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim registry = My.Computer.Registry
+        Dim mainkey = registry.CurrentUser.OpenSubKey("Software\MPC-HC\MPC-HC\MediaHistory")
+        For Each keyname As String In mainkey.GetSubKeyNames
+            Dim History = mainkey.OpenSubKey(keyname).GetValue("Filename")
+            Dim lastopen = mainkey.OpenSubKey(keyname).GetValue("LastOpened")
+            myDictionary.Add(lastopen, History)
+
+        Next
+        Dim sortedDictionary = myDictionary.OrderByDescending(Function(kvp) DateTime.Parse(kvp.Key)).ToDictionary(Function(kvp) kvp.Key, Function(kvp) kvp.Value)
+        For Each kvp In sortedDictionary
+            ListBox1.Items.Add(kvp.Value)
+        Next
+        HistoryCountLBL.Text = $"All :{ListBox1.Items.Count}"
+        mainkey.Close()
+    End Sub
+
+
+
+    Private Sub ClearBtn_Click(sender As Object, e As EventArgs) Handles ClearBtn.Click
+        If ListBox1.SelectedItems.Count > 0 Then
+            Dim registry = My.Computer.Registry
+            Dim mainkey = registry.CurrentUser.OpenSubKey("Software\MPC-HC\MPC-HC\MediaHistory", True)
+            For Each keyname As String In mainkey.GetSubKeyNames
+                Dim History = mainkey.OpenSubKey(keyname)
+                Dim filename As String = History.GetValue("Filename")
+                If ListBox1.SelectedItems.Contains(filename) Then
+
+                    mainkey.DeleteSubKey(keyname)
+                    ListBox1.Items.Remove(filename)
+                    ListBox1.Refresh()
+                End If
+            Next
+
+            mainkey.Close()
+        Else
+            MsgBox("Select Itesm to be removed from History")
+        End If
+
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        SelectedLBL.Text = $"Selected items: {ListBox1.SelectedItems.Count}"
+        HistoryCountLBL.Text = $"All :{ListBox1.Items.Count}"
+    End Sub
+End Class
